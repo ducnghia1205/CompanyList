b07ee015230a95b71d6851f6463c313b9e14ebf7
ducnghia <nghia.hoang@dinovative.com>
2019-08-17 15:53:22 +0700

    create table companies

--- bin/connections.js
+++ bin/connections.js
@@ -1,26 +1,26 @@
-require('dotenv').config();
-const PSQL = require('pg');
+require(`dotenv`).config();
+const PSQL = require(`pg`);
 const urlDataBase = process.env.user;
 const passwordDataBase = process.env.password;
 const portDataBase = process.env.dbPort;
 const host = process.env.HOST;
 const dbName = process.env.database;
+let client;
 
-console.log('process.env.user: ', process.env.user);
 module.exports = {
   // postgres
   getPortGresClientWrite: () => {
     const connectionString = `${urlDataBase}:${passwordDataBase}@${host}:${portDataBase}/${dbName}`;
-    const client = new PSQL.Client({
+    client = new PSQL.Client({
       connectionString: connectionString
     });
     client.connect()
       .then(success => {
         //success.done(); // success, release the connection;
-        console.log('connect to database postgres SQL success')
+        console.log(`connect to database postgres SQL success`)
     })
       .catch(error => {
-        console.log('ERROR:', error.message || error);
+        console.log(`ERROR:`, error.message || error);
       });
     global.PSQL = client;
   }
---
+++ migrations/20190817144117_createCompanies.js
@@ -0,0 +1,37 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable('companies', table => {
+    table.increments('id')
+      .notNullable()
+      .primary()
+
+    table.string('name')
+      .notNullable()
+
+    table.string('employee_count')
+      .nullable()
+
+    table.string('description')
+      .nullable()
+
+    table.string('country_code')
+      .notNullable()
+
+    table.string('address')
+      .notNullable()
+
+    table.string('placeholder_url')
+      .notNullable()
+
+    table.string('video_url')
+      .nullable()
+
+    table.string('founded_at')
+      .nullable()
+
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTable('companies');
+};
