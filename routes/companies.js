319fe719e510a838a08f51cf6745e8df0aef5210
ducnghia <nghia.hoang@dinovative.com>
2019-08-20 22:11:01 +0700

    Create API update delete company

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/db');
+const moment = require('moment');
 
 router.get('/', async function (req, res) {
   const perPage = req.query.per_page || 20;
@@ -25,11 +26,56 @@ router.post('/',async function (req, res) {
     const country_code = req.body.country_code;
     const address = req.body.address;
     const placeholder_url = req.body.placeholder_url;
-    
+
     if (!name || !country_code || !address || !placeholder_url) {
       return res.json('missing required field!');
     }
     await db('companies').insert(req.body);
+
+    return res.json('success');
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+router.put('/:id',async function (req, res) {
+  try {
+    const id = req.params.id;
+    const name = req.body.name;
+    const country_code = req.body.country_code;
+    const address = req.body.address;
+    const placeholder_url = req.body.placeholder_url;
+
+    if (!id || !name || !country_code || !address || !placeholder_url) {
+      return res.json('missing required field!');
+    }
+    const company = await db('companies').select('*').where('id', id);
+
+    if (!company || !company.length){
+      return res.json('The company does not exist.')
+    }
+    req.body.updated_at = moment().format('YYYY-MM-DD HH:mm:ss.ssssssZZ').slice(0, -2);
+    await db('companies').update(req.body).where('id', id);
+
+    return res.json('success');
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+router.delete('/:id',async function (req, res) {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      return res.json('missing required field!');
+    }
+    const company = await db('companies').select('*').where('id', id);
+
+    if (!company || !company.length){
+      return res.json('The company does not exist.')
+    }
+    await db('companies').update({'deleted_at': moment().format('YYYY-MM-DD HH:mm:ss.ssssssZZ').slice(0, -2)}).where('id', id);
 
     return res.json('success');
   } catch (e) {
