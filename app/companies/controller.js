daba8c024dc77f64f4832c176185f3a632159c34
ducnghia <nghia.hoang@dinovative.com>
2019-09-01 13:50:01 +0700

    Get single company

--- app/companies/controller.js
+++ app/companies/controller.js
@@ -1,18 +1,35 @@
 const db = require('../../db/db');
-const moment = require('moment');
 const {validationResult} = require('express-validator');
 const dateTimeUtils = require('../../helpers/dateTimeUtils');
 
 module.exports = {
+  getCompany: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const result = await db('companies').where({id: id}).first();
+
+      if (!result) {
+        return res.error(`company with id ${id} doesn't exit`)
+      }
+
+      return res.success(result);
+    } catch (e) {
+      return res.error(`failed to load company due to ${e.message}`);
+    }
+  },
   getListCompanies: async (req, res) => {
     try {
       const perPage = req.query.per_page || 20;
       const page = req.query.page || 1;
-      const result = await PSQL.query(
-        `SELECT * FROM companies LIMIT ${perPage} OFFSET ${(page - 1) * perPage}`
-      );
+      const result = await db('companies')
+        .select('*')
+        .limit(perPage).offset((page - 1) * perPage);
 
-      return res.success(result.rows);
+      if (!result){
+        return res.error('failed to load companies.');
+      }
+
+      return res.success(result);
     } catch (e) {
       return res.error(`failed to load companies due to ${e.message}`);
     }
@@ -42,13 +59,17 @@ module.exports = {
         return res.error({errors: errors.array()}, 422);
       }
 
-      let company = await db('companies').select('*').where('id', id).returning('*');
-      if (!company || !company.length) {
+      let company = await db('companies').select('*').where('id', id).first();
+      if (!company) {
         return res.error(`failed to update company.`);
       }
 
       req.body.updated_at = dateTimeUtils.formatDatetimePostgres();
       company = await db('companies').update(req.body).where('id', id).returning('*');
+
+      if (!company || !company.length) {
+        return res.error('failed to update company')
+      }
 
       return res.success(company[0]);
     } catch (e) {
@@ -64,13 +85,17 @@ module.exports = {
         return res.error({errors: errors.array()}, 422);
       }
 
-      let company = await db('companies').select('*').where('id', id);
-      if (!company || !company.length) {
+      let company = await db('companies').select('*').where('id', id).first();
+      if (!company) {
         return res.error(`failed to delete company.`);
       }
 
       company = await db('companies').update({'deleted_at': dateTimeUtils.formatDatetimePostgres()})
         .where('id', id).returning('*');
+
+      if (!company || !company.length) {
+        return res.error('failed to update company')
+      }
 
       return res.success(company[0]);
     } catch (e) {
