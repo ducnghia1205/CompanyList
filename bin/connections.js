2725b41f7aa0be2726966ccb2ef9984526b6a14d
Hoang Phat Nguyen <hoangphat@inspectorio.com>
2019-09-01 12:27:46 +0700

    Add seed file to add users

--- bin/connections.js
+++ bin/connections.js
@@ -1,4 +1,5 @@
 require(`dotenv`).config();
+
 const PSQL = require(`pg`);
 const urlDataBase = process.env.user;
 const passwordDataBase = process.env.password;
@@ -8,20 +9,20 @@ const dbName = process.env.database;
 let client;
 
 module.exports = {
-  // postgres
-  getPortGresClientWrite: () => {
-    const connectionString = `${urlDataBase}:${passwordDataBase}@${host}:${portDataBase}/${dbName}`;
-    client = new PSQL.Client({
-      connectionString: connectionString
-    });
-    client.connect()
-      .then(success => {
-        //success.done(); // success, release the connection;
-        console.log(`connect to database postgres SQL success`)
-    })
-      .catch(error => {
-        console.log(`ERROR:`, error.message || error);
-      });
-    global.PSQL = client;
-  }
+    // postgres
+    getPortGresClientWrite: () => {
+        const connectionString = `${urlDataBase}:${passwordDataBase}@${host}:${portDataBase}/${dbName}`;
+        client = new PSQL.Client({
+            connectionString: connectionString
+        });
+        client.connect()
+            .then(success => {
+                //success.done(); // success, release the connection;
+                console.log(`connect to database postgres SQL success`)
+            })
+            .catch(error => {
+                console.log(`ERROR:`, error.message || error);
+            });
+        global.PSQL = client;
+    }
 };
